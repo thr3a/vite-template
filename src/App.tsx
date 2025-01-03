6113e95d6460c71a1e6888928fb8f7465605e0e1
thr3a <thr3a@example.com>
2025-01-04 02:50:40 +0900

    update: タイトル追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
+import { Container, MantineProvider, Title } from '@mantine/core';
 import { ColorSchemeToggle } from './ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from './Welcome/Welcome';
 import { theme } from './theme';
@@ -7,8 +7,16 @@ import { theme } from './theme';
 export default function App() {
   return (
     <MantineProvider theme={theme}>
-      <Welcome />
-      <ColorSchemeToggle />
+      <Container id='container' maw={400}>
+        <Title mt={'sm'} order={2}>
+          Mantine Vite template
+        </Title>
+        <Title order={6} mb={'sm'} c={'dimmed'}>
+          Mantine Vite template
+        </Title>
+        <Welcome />
+        <ColorSchemeToggle />
+      </Container>
     </MantineProvider>
   );
 }
