c2bab476f61c9ec361a6a529f10c7f9758187d84
thr3a <thr3a@example.com>
2025-05-08 22:22:38 +0900

    clean:

--- README.md
+++ README.md
@@ -3,6 +3,3 @@
 ```bash
 node --import tsx --env-file .env --watch ./src/scripts/hello.ts
 ```
-
-react routerを導入して、/の場合は今まで通り、/aboutの場合はabout.tsxが表示されるようにして
-npm install許可する
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,17 +1,14 @@
-import { Container, MantineProvider, Title } from '@mantine/core';
-import { theme } from './theme';
+import { Container, Title } from '@mantine/core';
 
 export default function Home() {
   return (
-    <MantineProvider theme={theme}>
-      <Container maw={400}>
-        <Title mt={'sm'} order={2}>
-          Mantine Vite template
-        </Title>
-        <Title order={6} mb={'sm'} c={'dimmed'}>
-          Mantine Vite template
-        </Title>
-      </Container>
-    </MantineProvider>
+    <Container maw={400}>
+      <Title mt={'sm'} order={2}>
+        Mantine Vite template
+      </Title>
+      <Title order={6} mb={'sm'} c={'dimmed'}>
+        Mantine Vite template
+      </Title>
+    </Container>
   );
 }
